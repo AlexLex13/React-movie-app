8ec97cf1b2368d798376f73189b1861608ed3b93
Alexey <suxovarov.alexey@gmail.com>
2023-02-23 13:45:39 +0300

    add Route Reviews

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Routes, Route} from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
 
 function App() {
 
@@ -50,6 +51,7 @@ function App() {
                 <Route path="/" element={<Layout/>}>
                     <Route path="/" element={<Home movies={movies}/>}></Route>
                     <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+                    <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
                 </Route>
             </Routes>
 
